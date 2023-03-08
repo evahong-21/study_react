8396e50ef42de6537c525bcf40c8e149cc637822
jihyun hong <jihyunhong@jihyunui-MacBookAir.local>
2023-03-08 19:36:44 +0900

    3.NumberBaseball class component

--- react-webgame/3.NumberBaseball/NumberBaseball.jsx
+++ react-webgame/3.NumberBaseball/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback } from 'react';
+import React, { useRef, useState, useCallback, Component } from 'react';
 import Try from './Try'
 
 const getNumbers = () => {
@@ -11,37 +11,51 @@ const getNumbers = () => {
     return array;
   };
 
+class NumberBaseball extends Component {
+    state = {
+        result: '',
+        value: '',
+        answer: getNumbers(),
+        tries: [],
+    };
 
-const NumberBaseball = () => {
-    const [result, setResult] = useState(getNumbers());
-    const [value, setValue] = useState('');
-    const [tries, setTries] = useState([]);
-    const [answer, setAnswer] = useState('');
-    const inputEl = useRef(null);
-
-
-    const onSubmitForm = useCallback((e)=>{
+    onSubmitForm = (e) => {
         e.preventDefault();
-        console.log(result);
-        setTries([...tries, value]);
-        
-    })
+        if (this.state.value === this.state.answer.join('')) {
+            this.setState({result:'홈런',tries:[...this.state.tries, {try: this.state.value, result: '홈런!'}]})
+        } else {
+            if (this.state.tries.length >= 9) {
+                this.setState({result:'10번 넘게 탈락! 정답 :' + this.state.answer.join('')});
+                alert('게임을 다시 시작합니다.');
+                this.setState({answer:getNumbers()});
+            } else {            
+                const arr = this.state.value.split('');
+                let strike = 0;
+                let ball = 0;
+                console.log(arr, this.state.answer)
+                arr.map((v,i)=>{this.state.answer[i]==Number(v) ? strike++ : this.state.answer.includes(Number(v)) ? ball++ : null});
+                this.setState({result:'땡!', tries:[...this.state.tries, {try: this.state.value, result:`스트라이크 : ${strike}, 볼: ${ball}`}]})
+            }
+        }
+    };
 
-    const onChangeInput = useCallback((e)=> setValue(e.target.value), []);
+    onChangeForm = (e) => {
+        console.log(this.state.answer);
+        this.setState({
+            value: e.target.value,
+        });
+    };
 
-    return (
-        <>
-            <h1>NumberBaseball</h1>
-            <form onSubmit={onSubmitForm}>
-                <input maxLength={4} value={value} onChange={onChangeInput} />
-                <button>submit</button>
-            </form>
-            <div>tries: {tries.length}</div>
-            <ui>
-                {tries && tries.map((v, i) => {return(<Try value={v} index={i}/>)})}
-            </ui>
-        </>
-    );
-}
+    render() {
+        return (<><h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+            <input maxLength={4} value={this.state.value} onChange={this.onChangeForm} /> 
+        </form>
+        <div>시도 : {this.state.tries.length}</div>
+        <ul>
+            {this.state.tries.map((v,i)=>{return(<Try key={`${i+1}차 시도:`} value={v}/>)})}</ul>    
+        </>)
+    };
+};
 
-export default NumberBaseball
+export default NumberBaseball;
--- react-webgame/3.NumberBaseball/Try.jsx
+++ react-webgame/3.NumberBaseball/Try.jsx
@@ -1,23 +1,10 @@
-// import React, { memo } from 'react';
-
-// const Try = memo(({tryInfo}) => {
-//   return (
-//     <li>
-//       <div>{tryInfo.try}</div>
-//       <div>{tryInfo.result}</div>
-//     </li>
-//   );
-// });
-
-// export default Try;
-
 import React, { Component } from 'react';
 
 class Try extends Component {
     render() {
         return (
             <li>
-                <div>index : {this.props.index} value : {this.props.value}</div>
+                <div>try : {this.props.value.try} result : {this.props.value.result}</div>
             </li>
         )
     }
