eeb50fbac9cd18318502c9fdf11c29abee01d0e1
evahong-21 <evahong94@gmail.com>
2023-04-04 20:24:20 +0900

    6.TicTakTo useReducer - 1

---
+++ react-webgame/6.TicTakTo/Table.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Tr from "./Tr";
+
+const Table = ({onClick, tableData}) => {
+    return (
+        <table onClick={onClick}>
+            {Array(tableData.length).fill().map((tr, i) => <Tr rowData={tableData[i]}/>)}
+        </table>
+        
+    )
+}
+
+export default Table;
---
+++ react-webgame/6.TicTakTo/Tr.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Td from "./Td";
+
+const Tr = ({rowData}) => {
+    return (
+        <tr>
+            {Array(rowData.length).fill().map((td) => <Td>{''}</Td>)}
+        </tr>
+        
+    )
+}
+
+export default Tr;
