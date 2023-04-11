7848e62a7c1aa4e7277a3aecd0e414c413b9488e
evahong94 <evahong94@gmail.com>
2023-04-11 21:28:55 +0900

    6.Lotto function component & useMemo/useCallback

--- react-webgame/6.Lotto/client.jsx
+++ react-webgame/6.Lotto/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Lotto from './LottoClass.jsx';
+import Lotto from './Lotto.jsx';
 
 ReactDOM.render(<Lotto />, document.querySelector('#root'));
