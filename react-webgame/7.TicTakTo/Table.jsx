d518c2e1780ae84b63d7832c4ac4a816648f573d
evahong-21 <evahong94@gmail.com>
2023-04-12 18:12:51 +0900

    7.TicTakTo useEffect/useCallback/useMemo/React.memo

--- react-webgame/7.TicTakTo/Table.jsx
+++ react-webgame/7.TicTakTo/Table.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Tr from "./Tr";
 
-const Table = ({onClick, tableData}) => {
+const Table = ({ tableData, dispatch }) => {
     return (
-        <table onClick={onClick}>
-            {Array(tableData.length).fill().map((tr, i) => <Tr rowData={tableData[i]}/>)}
-        </table>
-        
-    )
-}
-
+      <table>
+        <tbody>
+          {Array(tableData.length).fill().map((tr, i) => (
+            <Tr key={i} dispatch={dispatch} rowIndex={i} rowData={tableData[i]} />
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+  
 export default Table;
